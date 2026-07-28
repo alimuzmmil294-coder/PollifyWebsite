c9f1a3666dc87d01ff5a904511f5ecf6a6608dbf
MUAZMMIL ALI <alimuzmmil294@gmail.com>
2026-07-29 00:08:16 +0500

    completed poll things

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import authRoutes from "./auth.js";
+import notificaionRoute from "./notification.js";
+import pollRoutes from "./poll.js";
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/notifications", notificaionRoute);
+router.use("/notifications", pollRoutes);
 
 export default router;
